df57d95524db0ba6f92ae02f7ae8e71487191495
ttrtegerer <tolyanivanov2002@mail.ru>
2022-07-15 01:17:04 +0300

    FirstVersion

---
+++ ApiCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,87 @@
+﻿using CalculatorViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost("Operation")]
+        public IActionResult Operation([FromBody] CalculatorView model)
+        {
+            if (IsNumeric(model.first_number) && IsNumeric(model.last_number))
+            {
+                switch (model.operation)
+                {
+                    case "summ":
+                        {
+                            var sum = ConvertToDecimal(model.first_number) + ConvertToDecimal(model.last_number);
+                            model.answer = sum;
+                            return Ok(model.answer);
+                        };
+                        
+                    case "multiply":
+                        {
+                            var mult = ConvertToDecimal(model.first_number) * ConvertToDecimal(model.last_number);
+                            model.answer = mult;
+                            return Ok(model.answer);
+                        };
+                    case "substract":
+                        {
+                            var sub = ConvertToDecimal(model.first_number) - ConvertToDecimal(model.last_number);
+                            model.answer = sub;
+                            return Ok(model.answer);
+                        };
+                    case "divide":
+                        {
+                            switch (model.last_number)
+                            {
+                                case "0":
+                                    {
+                                        return BadRequest("На ноль делить нельзя");
+                                    }
+                                default:
+                                    {
+                                        var div = ConvertToDecimal(model.first_number) / ConvertToDecimal(model.last_number);
+                                        model.answer = div;
+                                        return Ok(model.answer);
+                                    }
+                            }                  
+                            
+
+                        };
+                    default:
+                        return BadRequest("Неизвестная операция");
+
+                }
+            }
+
+            return BadRequest("Invalid Input");
+        }
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber
+                , System.Globalization.NumberStyles.Any
+                , System.Globalization.NumberFormatInfo.InvariantInfo
+                , out number);
+            return isNumber;
+        }
+    }
+}
---
+++ CalculatorViewModel/CalculatorView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculatorViewModel
+{
+    public class CalculatorView
+    {
+        [Required(ErrorMessage = "Введите Первую цифру")]
+        public string first_number { get; set; }
+
+        [Required(ErrorMessage = "Введите Вторую цифру")]
+        public string last_number { get; set; }
+
+        [Required(ErrorMessage = "Введите Операцию")]
+        public string operation { get; set; }
+
+        public decimal answer { get; set; }
+    }
+}
